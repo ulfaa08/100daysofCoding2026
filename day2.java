d6f7902299e8942d885b3d96a5132765c990d61a
ulfaa08 <zlfindriyani@gmail.com>
2026-09-03 18:59:01 +0800

    day2.java

---
+++ day2.java
@@ -0,0 +1,18 @@
+public class day2{
+    public static void main(String[] args) {
+
+        // Menggunakan println()
+        System.out.println("===== BIODATA DIRI =====");
+        System.out.println("Nama     : Zulfa Indriyani");
+
+        // Menggunakan print()
+        System.out.print("NIM      : ");
+        System.out.print("D0224526");
+        System.out.print("\n");
+
+        // Menggunakan printf()
+        System.out.printf("Semester : %d%n", 5);
+
+        System.out.println("========================");
+    }
+}
