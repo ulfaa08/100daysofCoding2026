f157755906b3dcde15a51cc35d9d3355aaf11d15
ulfaa08 <zlfindriyani@gmail.com>
2026-09-04 21:32:03 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,24 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+
+        System.out.println("Nama\t: Zulfa ");
+        System.out.println("NIM\t: D0224526");
+        System.out.println("Kelas\t: Day3");
+
+        System.out.println("\n=== Data Mahasiswa ===");
+
+        System.out.println("Nama saya adalah \"Zulfa \"");
+        System.out.println("mahasiswa Informatika.");
+        System.out.println("belajar Java.");
+
+        System.out.println("\nMata Kuliah:\tPemrograman");
+        System.out.println("Materi:\t\tEscape Sequence");
+
+        System.out.println("\nContoh escape sequence:");
+        System.out.println("\\n = Membuat baris baru");
+        System.out.println("\\t = Membuat tab/spasi");
+        System.out.println("\\\" = Menampilkan tanda kutip");
+        System.out.println("\\\\ = Menampilkan tanda backslash");
+        System.out.println("\\' = Menampilkan tanda petik satu");
+    }
+}
