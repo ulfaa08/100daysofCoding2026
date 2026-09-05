77c1b9e5b1bc8ddc78ec8616a22e61955c4f3619
ulfaa08 <zlfindriyani@gmail.com>
2026-09-05 22:44:22 +0800

    day4.java

---
+++ day4.java
@@ -0,0 +1,21 @@
+ public class TipeDataNumerikBilanganBulat {
+    public static void main(String[] args) {
+        // 4 tipe data numerik bilangan bulat
+        byte umur = 20;              // byte
+        short tahunMasuk = 2024;     // short
+        int nim = 0224526;           // int
+        long noRekening = 1234567890L; // long, wajib ada L
+
+        System.out.println("========== DATA MAHASISWA ==========");
+        System.out.println("Nama   : Zulfa ");
+        System.out.println("NIM    : D0224526");
+        System.out.println("Topik  : Tipe Data Numerik Bilangan Bulat");
+        System.out.println("====================================");
+        
+        System.out.println("Contoh byte  : Umur = " + umur + " tahun");
+        System.out.println("Contoh short : Tahun Masuk = " + tahunMasuk);
+        System.out.println("Contoh int   : NIM = " + nim);
+        System.out.println("Contoh long  : No Rekening = " + noRekening);
+        System.out.println("====================================");
+    }
+}
