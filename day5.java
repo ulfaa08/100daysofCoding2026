7eb02eef27825037f348a7babcecd952f09b34c1
ulfaa08 <zlfindriyani@gmail.com>
2026-09-06 23:04:21 +0800

    day5.java

---
+++ day5.java
@@ -0,0 +1,24 @@
+public class TipeDataPecahan {
+    public static void main(String[] args) {
+        // Deklarasi variabel tipe data pecahan
+        float berat = 52.5f;        // float pakai f
+        float harga = 45000.99f;    
+        double tinggi = 160.75;     // double
+        double ipk = 3.85;          
+
+        System.out.println("========== DATA MAHASISWA ==========");
+        System.out.println("Nama  : Zulfa");
+        System.out.println("NIM   : D0224526");
+        System.out.println("Topik : Tipe Data Pecahan (double, float)");
+        System.out.println("====================================");
+        
+        System.out.println("Contoh float  : ");
+        System.out.println("Berat = " + berat + " kg");
+        System.out.println("Harga = Rp " + harga);
+        
+        System.out.println("Contoh double : ");
+        System.out.println("Tinggi = " + tinggi + " cm");
+        System.out.println("IPK = " + ipk);
+        System.out.println("====================================");
+    }
+}
