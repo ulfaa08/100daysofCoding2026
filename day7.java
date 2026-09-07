a52a91cabc27a47d0fe467fc5ebd76184c7d741c
ulfaa08 <zlfindriyani@gmail.com>
2026-09-08 00:50:15 +0800

    day7.java

---
+++ day7.java
@@ -0,0 +1,29 @@
+public class pengenalanString {
+  public static void main (string[] args) { 
+     // 1. DEKLARASI TIPE DATA STRING 
+     string nama = "ulfa";
+     string nim ="D0224526";
+     string kampus = "Universitas sulawesi barat";
+     string hobi = "Badminton";
+     System.out.println("=== PENGENALAN TIPE DATA STRING ===");
+     System.out.println("Nama  :" + nama); 
+     System.out.prinln ("NIM   :" + nim);
+     System.out.println("kampus:" + kampus);
+     System.out.println("Hobi  :" + hobi);
+    // 2. MENGGABUNGKAN STRING = KONKATENASI 
+    String biodata ="perkenalkan nama saya" + nama +
+                    " NIM " + nim + 
+                    " dari " + kampus;
+    System.out.prinln("\nBiodata: " + biodata);
+    // 3. METHOD YANG SERING DIPAKE DI STRING 
+    System.out.println("\n=== METHOD STRING ==="):
+    System.out.println("jumlah karakter nama: " + nama.length());
+    System.out.println("Nama huruf besar    : " + nama.toUpperCase());
+    System. out.println("kampus huruf kecil : " + kampus.tolowerCase());
+  }
+}
+                    
+    
+    
+    
+    day 7
