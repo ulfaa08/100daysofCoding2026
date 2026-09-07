ad108d8c5123f5557c4d379aabbcef41702f9c40
ulfaa08 <zlfindriyani@gmail.com>
2026-09-07 22:40:24 +0800

    day6.java

---
+++ day6.java
@@ -0,0 +1,24 @@
+public class TipeDataCharBoolean {
+  public static void main(String[] args) { 
+
+  // 1. TIPE DATA CHAR = 1 karakter, pakai 'petik satu'
+   char huruf = 'A';
+   char angka = '7';
+   char simbol= '#';
+    System.out.println("=== TIPE DATA CHAR ===");
+    System.out.pritln("Huruf: " + huruf);
+    System.out.println("Angka: " + angka);
+    System.out.println("simbol: " + simbol);
+
+  // 2. TIPE DATA BOOLEAN = true atau false 
+  boolean lulus = true; 
+  boolean hujan = false;
+    int nilai = 85;
+    boolean lulusujian = nilai >=75; //hasil perbandingan 
+
+  System.out.println("\n=== TIPE DATA BOOLEAN ===");
+  System.out.println("status lulus: " + lulus);
+  System.out.println("Hari ini hujan:  " + hujan);
+  System.out.println("Nilai " + nilai + ",Lulus ujian? " + lulusUjian);
+  }
+}
