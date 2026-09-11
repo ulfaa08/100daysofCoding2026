e449ba0c33a1fc50270a8827bcbfcc0f17128b9a
ulfaa08 <zlfindriyani@gmail.com>
2026-09-11 20:17:59 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,10 @@
+public class BatasTipeData {
+    public static void main(String[] args) {
+        System.out.println("INT    -> Min: " + Integer.MIN_VALUE + " | Max: " + Integer.MAX_VALUE);
+        System.out.println("LONG   -> Min: " + Long.MIN_VALUE + " | Max: " + Long.MAX_VALUE);
+        System.out.println("FLOAT  -> Min: " + Float.MIN_VALUE + " | Max: " + Float.MAX_VALUE);
+        System.out.println("DOUBLE -> Min: " + Double.MIN_VALUE + " | Max: " + Double.MAX_VALUE);
+        System.out.println("BYTE   -> Min: " + Byte.MIN_VALUE + " | Max: " + Byte.MAX_VALUE);
+        System.out.println("CHAR   -> Min: " + (int)Character.MIN_VALUE + " | Max: " + (int)Character.MAX_VALUE);
+    }
+}
