30a2dc5f49cd4544c7750dfc1929ea65935ac06f
ulfaa08 <zlfindriyani@gmail.com>
2026-09-15 22:36:42 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,16 @@
+public class OperatorTambahKurang {
+    public static void main(String[] args) {
+        // angkanya langsung ditulis di sini
+        int angka1 = 50;
+        int angka2 = 10;
+        
+        int tambah = angka1 + angka2;
+        int kurang = angka1 - angka2;
+        
+        System.out.println("Angka pertama: " + angka1);
+        System.out.println("Angka kedua: " + angka2);
+        System.out.println("------------------------");
+        System.out.println("Penjumlahan (+) : " + angka1 + " + " + angka2 + " = " + tambah);
+        System.out.println("Pengurangan (-) : " + angka1 + " - " + angka2 + " = " + kurang);
+    }
+}
